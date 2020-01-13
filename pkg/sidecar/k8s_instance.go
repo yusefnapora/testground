91e7cabbc56e3cea9d76c92dc876cb1d30159465
Anton Evangelatov <anton.evangelatov@gmail.com>
2020-01-13 10:53:32 +0100

    wip\

--- pkg/sidecar/k8s_instance.go
+++ pkg/sidecar/k8s_instance.go
@@ -8,9 +8,9 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"path/filepath"
 
-	"github.com/docker/docker/api/types"
-	"github.com/docker/docker/api/types/filters"
+	"github.com/containernetworking/cni/libcni"
 	"github.com/vishvananda/netlink"
 	"github.com/vishvananda/netns"
 
@@ -72,25 +72,14 @@ func (d *K8sInstanceManager) manageContainer(ctx context.Context, container *doc
 		return nil, fmt.Errorf("inspect failed: %w", err)
 	}
 
+	// Initialise CNI config
+	cninet := libcni.NewCNIConfig(filepath.SplitList("/host/opt/cni/bin"), nil)
+
 	if !info.State.Running {
 		return nil, fmt.Errorf("not running")
 	}
 
-	// TODO: cache this?
-	networks, err := container.Manager.NetworkList(ctx, types.NetworkListOptions{
-		Filters: filters.NewArgs(
-			filters.Arg(
-				"label",
-				"testground.runid="+info.Config.Labels["testground.runid"],
-			),
-		),
-	})
-	if err != nil {
-		return nil, fmt.Errorf("failed to list networks: %w", err)
-	}
-
 	// Construct the runtime environment
-
 	runenv, err := runtime.ParseRunEnv(info.Config.Env)
 	if err != nil {
 		return nil, fmt.Errorf("failed to parse run environment: %w", err)
@@ -114,85 +103,77 @@ func (d *K8sInstanceManager) manageContainer(ctx context.Context, container *doc
 		}
 	}()
 
-	// Map _current_ networks to links.
-	links, err := dockerLinks(netlinkHandle, info.NetworkSettings)
-	if err != nil {
-		return nil, fmt.Errorf("failed to enumerate links: %w", err)
-	}
-
 	// Finally, construct the network manager.
-	network := &DockerNetwork{
-		container:      container,
-		activeLinks:    make(map[string]*dockerLink, len(info.NetworkSettings.Networks)),
-		availableLinks: make(map[string]string, len(networks)),
-		nl:             netlinkHandle,
+	network := &K8sNetwork{
+		netnsPath:   fmt.Sprintf("/proc/%d/ns/net", info.State.Pid),
+		cninet:      cninet,
+		container:   container,
+		subnet:      runenv.TestSubnet.String(),
+		activeLinks: make(map[string]*k8sLink, len(info.NetworkSettings.Networks)),
+		nl:          netlinkHandle,
 	}
 
-	for _, n := range networks {
-		name := n.Labels["testground.name"]
-		id := n.ID
-		network.availableLinks[name] = id
-	}
+	// TODO(anteva): remove all routes but redis and the data subnet
 
-	reverseIndex := make(map[string]string, len(network.availableLinks))
-	for name, id := range network.availableLinks {
-		reverseIndex[id] = name
-	}
+	//reverseIndex := make(map[string]string, len(network.availableLinks))
+	//for name, id := range network.availableLinks {
+	//reverseIndex[id] = name
+	//}
 
-	// TODO: Some of this code could be factored out into helpers.
+	//// TODO: Some of this code could be factored out into helpers.
 
-	// Get the routes to redis. We need to keep these.
-	redisRoutes, err := netlinkHandle.RouteGet(d.redis)
-	if err != nil {
-		return nil, fmt.Errorf("failed to resolve route to redis: %w", err)
-	}
+	//// Get the routes to redis. We need to keep these.
+	//redisRoutes, err := netlinkHandle.RouteGet(d.redis)
+	//if err != nil {
+	//return nil, fmt.Errorf("failed to resolve route to redis: %w", err)
+	//}
 
-	for id, link := range links {
-		if name, ok := reverseIndex[id]; ok {
-			// manage this network
-			handle, err := NewNetlinkLink(netlinkHandle, link.Link)
-			if err != nil {
-				return nil, fmt.Errorf(
-					"failed to initialize link %s (%s): %w",
-					name,
-					link.Attrs().Name,
-					err,
-				)
-			}
-			network.activeLinks[name] = &dockerLink{
-				NetlinkLink: handle,
-				IPv4:        link.IPv4,
-				IPv6:        link.IPv6,
-			}
-			continue
-		}
+	//for id, link := range links {
+	//if name, ok := reverseIndex[id]; ok {
+	//// manage this network
+	//handle, err := NewNetlinkLink(netlinkHandle, link.Link)
+	//if err != nil {
+	//return nil, fmt.Errorf(
+	//"failed to initialize link %s (%s): %w",
+	//name,
+	//link.Attrs().Name,
+	//err,
+	//)
+	//}
+	//network.activeLinks[name] = &dockerLink{
+	//NetlinkLink: handle,
+	//IPv4:        link.IPv4,
+	//IPv6:        link.IPv6,
+	//}
+	//continue
+	//}
 
-		// We've found a control network (or some other network).
+	//// We've found a control network (or some other network).
 
-		// Get the current routes.
-		linkRoutes, err := netlinkHandle.RouteList(link, netlink.FAMILY_ALL)
-		if err != nil {
-			return nil, fmt.Errorf("failed to list routes for link %s", link.Attrs().Name)
-		}
+	//// Get the current routes.
+	//linkRoutes, err := netlinkHandle.RouteList(link, netlink.FAMILY_ALL)
+	//if err != nil {
+	//return nil, fmt.Errorf("failed to list routes for link %s", link.Attrs().Name)
+	//}
 
-		// Add specific routes to redis if redis uses this link.
-		for _, route := range redisRoutes {
-			if route.LinkIndex != link.Attrs().Index {
-				continue
-			}
-			if err := netlinkHandle.RouteAdd(&route); err != nil {
-				return nil, fmt.Errorf("failed to add new route: %w", err)
-			}
-			break
-		}
+	//// Add specific routes to redis if redis uses this link.
+	//for _, route := range redisRoutes {
+	//if route.LinkIndex != link.Attrs().Index {
+	//continue
+	//}
+	//if err := netlinkHandle.RouteAdd(&route); err != nil {
+	//return nil, fmt.Errorf("failed to add new route: %w", err)
+	//}
+	//break
+	//}
 
-		// Remove the original routes
-		for _, route := range linkRoutes {
-			if err := netlinkHandle.RouteDel(&route); err != nil {
-				return nil, fmt.Errorf("failed to remove existing route: %w", err)
-			}
-		}
-	}
+	//// Remove the original routes
+	//for _, route := range linkRoutes {
+	//if err := netlinkHandle.RouteDel(&route); err != nil {
+	//return nil, fmt.Errorf("failed to remove existing route: %w", err)
+	//}
+	//}
+	//}
 	return NewInstance(runenv, info.Config.Hostname, network)
 }
 
@@ -218,14 +199,125 @@ func localAddresses() {
 	}
 }
 
-type K8sNetwork struct {
+type k8sLink struct {
+	*NetlinkLink
+	IPv4, IPv6 *net.IPNet
+
+	rt      *libcni.RuntimeConf
+	netconf *libcni.NetworkConfigList
 }
 
-func (n *K8sNetwork) Close() {
+type K8sNetwork struct {
+	container   *dockermanager.Container
+	activeLinks map[string]*k8sLink
+	nl          *netlink.Handle
+	cninet      *libcni.CNIConfig
+	subnet      string
+	netnsPath   string
+}
+
+func (n *K8sNetwork) Close() error {
+	return nil
 }
 
 func (n *K8sNetwork) ConfigureNetwork(ctx context.Context, cfg *sync.NetworkConfig) error {
-	return errors.New("not implemented")
+	if cfg.Network != "default" {
+		return errors.New("configured network is not default")
+	}
+
+	link, online := n.activeLinks[cfg.Network]
+
+	// Are we _disabling_ the network?
+	if !cfg.Enable {
+		// Yes, is it already disabled?
+		if online {
+			// No. Disconnect.
+			if err := n.cninet.DelNetworkList(ctx, link.netconf, link.rt); err != nil {
+				return err
+			}
+			delete(n.activeLinks, cfg.Network)
+		}
+		return nil
+	}
+
+	if online && ((cfg.IPv6 != nil && !link.IPv6.IP.Equal(cfg.IPv6.IP)) ||
+		(cfg.IPv4 != nil && !link.IPv4.IP.Equal(cfg.IPv4.IP))) {
+		// Disconnect and reconnect to change the IP addresses.
+		//
+		// NOTE: We probably don't need to do this on local docker.
+		// However, we probably do with swarm.
+		online = false
+		if err := n.cninet.DelNetworkList(ctx, link.netconf, link.rt); err != nil {
+			return err
+		}
+		delete(n.activeLinks, cfg.Network)
+	}
+
+	// Are we _connected_ to the network.
+	if !online {
+		// No, we're not.
+		// Connect.
+		netconf, err := buildNCL("net:" + n.subnet)
+
+		if cfg.IPv4 != nil {
+			netconf, err = buildNCL(cfg.IPv4.IP.String())
+		}
+		if cfg.IPv6 != nil {
+			return errors.New("ipv6 not supported")
+		}
+
+		cniArgs := [][2]string{}                   // empty
+		capabilityArgs := map[string]interface{}{} // empty
+		ifName := "eth1"
+
+		rt := &libcni.RuntimeConf{
+			ContainerID:    n.container.ID,
+			NetNS:          n.netnsPath,
+			IfName:         ifName,
+			Args:           cniArgs,
+			CapabilityArgs: capabilityArgs,
+		}
+
+		_, err = n.cninet.AddNetworkList(ctx, netconf, rt)
+		if err != nil {
+			return err
+		}
+
+		netlinkByName, err := n.nl.LinkByName(ifName)
+		if err != nil {
+			return err
+		}
+
+		// Register an active link.
+		handle, err := NewNetlinkLink(n.nl, netlinkByName)
+		if err != nil {
+			return err
+		}
+		v4addrs, err := handle.ListV4()
+		if err != nil {
+			return err
+		}
+		if len(v4addrs) != 1 {
+			return errors.New("expected 1 v4addrs")
+		}
+
+		link = &k8sLink{
+			NetlinkLink: handle,
+			IPv4:        v4addrs[0],
+			IPv6:        nil,
+		}
+		n.activeLinks[cfg.Network] = link
+	}
+
+	// We don't yet support applying per-subnet rules.
+	if len(cfg.Rules) != 0 {
+		return fmt.Errorf("TODO: per-subnet bandwidth rules not supported")
+	}
+
+	if err := link.Shape(cfg.Default); err != nil {
+		return err
+	}
+	return nil
 }
 
 func (n *K8sNetwork) ListActive() []string {
@@ -234,4 +326,80 @@ func (n *K8sNetwork) ListActive() []string {
 
 func (n *K8sNetwork) ListAvailable() []string {
 	return []string{}
+}
+
+func attach(addr string, netns string, containerID string, ifName string) error {
+	// <addr>     = [ip:]<cidr> | net:<cidr> | net:default
+
+	// addr := "net:10.36.79.0/24"
+	// ip := "10.36.79.10/24"
+	// netns := "" // abs path
+	// containerID := ""
+	// ifName := "net1"
+
+	// we assume that this is mapped as a volume
+	cniBinPath := filepath.SplitList("/host/opt/cni/bin")
+
+	bytes := []byte(`
+{
+    "cniVersion": "0.3.0",
+    "name": "weave",
+    "plugins": [
+        {
+            "name": "weave",
+            "type": "weave-net",
+						"ipam": {
+								"subnet": "` + addr + `"
+						},
+            "hairpinMode": true
+        }
+    ]
+}
+`)
+
+	netconf, err := libcni.ConfListFromBytes(bytes)
+	if err != nil {
+		return err
+	}
+
+	cniArgs := [][2]string{}                   // empty
+	capabilityArgs := map[string]interface{}{} // empty
+
+	cninet := libcni.NewCNIConfig(cniBinPath, nil)
+
+	rt := &libcni.RuntimeConf{
+		ContainerID:    containerID,
+		NetNS:          netns,
+		IfName:         ifName,
+		Args:           cniArgs,
+		CapabilityArgs: capabilityArgs,
+	}
+
+	_, err = cninet.AddNetworkList(context.TODO(), netconf, rt)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func buildNCL(addr string) (*libcni.NetworkConfigList, error) {
+	bytes := []byte(`
+{
+    "cniVersion": "0.3.0",
+    "name": "weave",
+    "plugins": [
+        {
+            "name": "weave",
+            "type": "weave-net",
+						"ipam": {
+								"subnet": "` + addr + `"
+						},
+            "hairpinMode": true
+        }
+    ]
+}
+`)
+
+	return libcni.ConfListFromBytes(bytes)
 }
